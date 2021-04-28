166edcd005c3cfb4eabb2e4e5d4bf9dfd7db688c
Adam <cf.eddie@gmail.com>
2021-04-28 08:30:28 -0700

    initial commit

---
+++ src/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DeckList from "../deck/list";
+
+function Home() {
+  return (
+    <div className="Home container">
+      <DeckList />
+    </div>
+  );
+}
+
+export default Home;
